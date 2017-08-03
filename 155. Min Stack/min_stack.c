3f68c4ec69718f8f767c1372d3d3b730af78da92
Ken Kuan <kuanken.cs96@g2.nctu.edu.tw>
2017-08-03 20:01:17 +0800

    [Data Structure] Stack implementation

---
+++ 155. Min Stack/min_stack.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct min_stack_s {
+    int size;
+    int head_idx;
+    int *head_ptr;
+    int *stack;
+} min_stack_t;
+
+min_stack_t* minStackCreate(int maxSize) {
+
+}
+
+void minStackPush(min_stack_t *obj, int x) {
+
+}
+
+void minStackPop(min_stack_t *obj) {
+
+}
+
+int minStackTop(min_stack_t *obj) {
+
+}
+
+int minStackGetMin(min_stack_t *obj) {
+
+}
+
+void minStackFree(min_stack_t *obj) {
+
+}
+
+int main()
+{
+    return 0;
+}
